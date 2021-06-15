412631bf4f3d8f01141d3716a0203d597ab52a14
Dwipasca <dpnursanti@gmail.com>
2021-06-15 15:31:13 +0800

    convert task1 from js to ts

--- src/task1.ts
+++ src/task1.ts
@@ -1,20 +1,22 @@
-/**
- * @param {object} cartItem
- * @param {object} cartItem.product
- * @param {string} cartItem.product.name
- * @param {number} cartItem.product.price
- * @param {number} cartItem.qty
- */
-function getSubtotal(cartItem) {
+interface Cart {
+  items: Array<CartItem>;
+}
+
+interface CartItem {
+  product: Product;
+  qty: number;
+}
+
+interface Product {
+  name?: string;
+  price: number;
+}
+
+function getSubtotal(cartItem: CartItem): number {
   return cartItem.product.price * cartItem.qty;
 }
 
-/**
- * @param {object} cart
- * @param {Array<object>} cart.items
- * @return {number}
- */
-function getCartTotal(cart) {
+function getCartTotal(cart: Cart): number {
   return cart.items
     .map((item) => getSubtotal(item))
     .reduce((total, price) => total + price, 0);
